84dbcc0052d4cbf0979534b30a96e9cba9612661
snwmds <snwmds@protonmail.com>
2020-10-21 15:08:23 -0300

    change rules file url

--- external/update_rules_file.py
+++ external/update_rules_file.py
@@ -4,7 +4,7 @@ from urllib.parse import urlparse
 from unalix._http import create_connection, get_encoded_content
 from unalix._config import headers, timeout
 
-rules_url = "https://kevinroebert.gitlab.io/ClearUrls/data/data.min.json"
+rules_url = "https://rules1.clearurls.xyz/data/data.minify.json"
 rules_path = "unalix/package_data/data.min.json"
 
 scheme, netloc, path, params, query, fragment = urlparse(rules_url)
