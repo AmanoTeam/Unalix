04784097c2683e2c902eae0a27063dc8ee7e022e
SnwMds <46784020+SnwMds@users.noreply.github.com>
2021-05-04 22:06:35 -0300

    add PyPy classifier to setup.py

--- setup.py
+++ setup.py
@@ -58,7 +58,8 @@ classifiers = [
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
-    "Programming Language :: Python :: 3.10"
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: Implementation :: PyPy"
 ]
 
 bad_keys = (
