9cefad7b50882315c474784ace8720d8b8559e3f
SnwMds <46784020+SnwMds@users.noreply.github.com>
2021-06-11 20:11:50 -0300

    bump version

--- unalix/__version__.py
+++ unalix/__version__.py
@@ -1,4 +1,4 @@
 __title__ = "Unalix"
 __description__ = "Small, dependency-free, fast Python package for removing tracking fields from URLs."
-__version__ = "0.6"
+__version__ = "0.7"
 __homepage__ = "https://github.com/AmanoTeam/Unalix"
